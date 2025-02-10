25377fdb61fd0caf0534976306aa2ce160624012
G Manjunth <manjudba1222@gmail.com>
2025-02-10 11:24:27 +0530

    hi

--- output.tf
+++ output.tf
@@ -2,3 +2,18 @@ output "vpc_id" {
     value = aws_vpc.main.id
   
 }
+
+output "public_subnet" {
+    value = aws_subnet.public_subnet.id
+  
+}
+
+output "private_subnet_subnet" {
+    value = aws_subnet.private_subnet.id
+  
+}
+
+output "database_subnet" {
+    value = aws_subnet.database_subnet.id
+  
+}
